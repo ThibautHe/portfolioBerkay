bf6bc40bfab3b155d42ca015e1c6d0964b977ed8
Tea-bow <thibaut.hellinckx@hotmail.com>
2024-07-01 15:11:17 +0200

    changed text

--- src/App.jsx
+++ src/App.jsx
@@ -51,177 +51,169 @@ const About = () => {
     // Add more software data as needed
   ];
   return (
-          <div>
-            <div
-              className="MainContentContainer"
-              style={{ backgroundColor: theme, color: readableColor }}
-            >
-              <div className="mainArticle">
-                <h1 className="mainTitle">
-                  Berkay <span className="mainTitleSpan">Erdemir.</span>
-                </h1>
-                <p className="mainDescription">
-                  I am a passionate Motion Designer with a strong foundation in
-                  Adobe After Effects and Premiere Pro, backed by a diploma in
-                  Media Design and Communications from Erasmus Hogeschool.
-                  Majoring in Motion has equipped me with the skills and
-                  knowledge to bring dynamic and captivating visual stories to
-                  life. Throughout my academic and professional journey, I have
-                  honed my expertise in creating visually compelling motion
-                  graphics, engaging video content, seamless animations and
-                  resonate with audiences
-                </p>
-                <img
-                  className="profilepic"
-                  style={{
-                    position: "absolute",
-                    right: "0",
-                    bottom: "0",
-                    width: "70%",
-                  }}
-                  src="image00001.png"
-                  alt=""
-                />
-                <Slider isVertical={false} hasEffect={true} isRelative={true} />
-              </div>
-              {/* <Slider
+    <div>
+      <div
+        className="MainContentContainer"
+        style={{ backgroundColor: theme, color: readableColor }}
+      >
+        <div className="mainArticle">
+          <h1 className="mainTitle">
+            Berkay <span className="mainTitleSpan">Erdemir.</span>
+          </h1>
+          <p className="mainDescription">
+            I am a passionate Motion Designer with a strong foundation in Adobe
+            After Effects and Premiere Pro, backed by a diploma in Media Design
+            and Communications from Erasmus Hogeschool. Majoring in Motion has
+            equipped me with the skills and knowledge to bring dynamic and
+            captivating visual stories to life. Throughout my academic and
+            professional journey, I have honed my expertise in creating visually
+            compelling motion graphics, engaging video content, seamless
+            animations and resonate with audiences
+          </p>
+          <img
+            className="profilepic"
+            style={{
+              position: "absolute",
+              right: "0",
+              bottom: "0",
+              width: "70%",
+            }}
+            src="image00001.png"
+            alt=""
+          />
+          <Slider isVertical={false} hasEffect={true} isRelative={true} />
+        </div>
+        {/* <Slider
           isVertical={true}
           hasEffect={false}
           offsetLeft={"80%"}
           isRelative={true}
           sliderHeight={"35%"}
           /> */}
-            </div>
+      </div>
 
-            <div
-              className="main-story-container"
-              style={{ color: `rgb(${color2.join(",")})` }}
-            >
-              <div className="main-story-article">
-                <div className="main-story-video-div">
-                  <video
-                    className="main-video"
-                    loop
-                    autoPlay
-                    muted
-                    preload="auto"
-                    playsInline
-                    src="https://d1iu13wka1lg1b.cloudfront.net/ShowreelFiverr.mp4"
-                    alt=""
-                  />
-                </div>
-                <div className="main-story-content">
-                  <h1 className={`main-story-title ${FontsCSS.regular}`}>
-                    how I started to like <br />
-                    <span
-                      className={`${FontsCSS.yellow} ${FontsCSS.boldOblique}`}
-                    >
-                      Motion Graphics
-                    </span>{" "}
-                    and <span className={`${FontsCSS.boldOblique}`}>Vfx</span>
-                  </h1>
-                  <p className="main-story-description">
-                    I pursued my studies in Multimedia and Communication
-                    Technologies at Erasmus Hogeschool Brussel, where I gained a
-                    comprehensive education that encompassed various facets of
-                    digital media. The curriculum included web design and
-                    programming, which equipped me with the skills to create
-                    responsive and visually appealing websites using HTML, CSS,
-                    and JavaScript. Working in big groups was a significant part
-                    of the program, fostering collaboration and project
-                    management skills essential for real-world applications. My
-                    passion for visual storytelling was nurtured through
-                    extensive training in video editing and motion graphics,
-                    where I learned to use industry-standard software like Adobe
-                    Premiere Pro and After Effects. This diverse educational
-                    experience not only broadened my technical abilities but
-                    also enhanced my creative thinking and problem-solving
-                    skills, preparing me for a dynamic career in the multimedia
-                    and communication technology fields.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div
-              className="education-container"
-              style={{ backgroundColor: theme }}
-            >
-              <Slider
-                isVertical={true}
-                offsetLeft={"50%"}
-                isRelative={true}
-                sliderHeight={"100px"}
-                BallPos={100}
-              />
-              <h1 className="education-title" style={{ color: readableColor }}>
-                my education
-              </h1>
-              <div className="education-line" />
-              <div className="education-content-container">
-                <div className="education-content">
-                  <h2 className="education-school">
-                    erasmus hogeschool brussels
-                  </h2>
-                  <h3 className="education-direction">
-                    <span className="bold">multimedia</span> &{" "}
-                    <span className="bold"> creative technologies</span>
-                  </h3>
-                  <p className="education-description">
-                    During my bachelor I got the opportunity to discover not
-                    only the development but also the design part. The studies
-                    were a fine line of balance between design and development.
-                    I would lie if I said that I didn't have my preferences.
-                    Indeed I felt way more comfortable with development than
-                    design. But I'm thankfull to have at least some bases in
-                    design. Along the road we had the opportunity to chose a
-                    path. it wasnt possible to chose all of them so I chose the
-                    game development course along with the other courses where I
-                    could explore this mystery of how game were made that kept
-                    me awake at night since I was a kid.
-                    <br />
-                    <br />
-                    What I mostly learned during these studies is that I'm far
-                    from knowing everything there is to know. I learned to
-                    learn, and to keep learning every day, that's what they
-                    called "lifelong learning". It was very autonmous learnig
-                    which wasnt always easy but at least I know now that it is
-                    possible.
-                  </p>
-                  <img className="education-img" src="/ehb.png" alt="" />
-                </div>
-              </div>
-            </div>
-
-            <div
-              className="softwares-container"
-              style={{ backgroundColor: theme, color: readableColor }}
-            >
-              <div className="softwares-content">
-                <h1>
-                  <span className="bold">software</span>
-                  <span className="light"> I use</span>
-                </h1>
-                <div
-                  className="pf-hr grid-2-span-col"
-                  style={{ width: "40%", height: "1px" }}
-                />
-              </div>
-              <AnimatedDiv className="softwares-logo" staggerchildrens={true}>
-                {softwareData.map((software, index) => (
-                  <SoftwareCard
-                    key={software.id} // Add a unique key for each item in the array
-                    logoSrc={software.logoSrc}
-                    softwareName={software.softwareName}
-                    type={software.type}
-                    whileInView={{ y: [-50, 0] }}
-                    transition={{ delay: 0.2 + index * 0.1 }}
-                  />
-                ))}
-              </AnimatedDiv>
-            </div>
-            <Footer />
+      <div
+        className="main-story-container"
+        style={{ color: `rgb(${color2.join(",")})` }}
+      >
+        <div className="main-story-article">
+          <div className="main-story-video-div">
+            <video
+              className="main-video"
+              loop
+              autoPlay
+              muted
+              preload="auto"
+              playsInline
+              src="https://d1iu13wka1lg1b.cloudfront.net/ShowreelFiverr.mp4"
+              alt=""
+            />
           </div>
+          <div className="main-story-content">
+            <h1 className={`main-story-title ${FontsCSS.regular}`}>
+              how I started to like <br />
+              <span className={`${FontsCSS.yellow} ${FontsCSS.boldOblique}`}>
+                Motion Graphics
+              </span>{" "}
+              and <span className={`${FontsCSS.boldOblique}`}>Vfx</span>
+            </h1>
+            <p className="main-story-description">
+              I've always been fascinated by the magic of visual effects (VFX),
+              captivated by the way they bring fantastical worlds and characters
+              to life in movies and games. From a young age, I was enthralled by
+              the stunning VFX I saw on screen, whether it was the intricate
+              details in blockbuster films or the immersive environments in
+              video games. This fascination spurred my curiosity about the VFX
+              workflow and the intricate process of creating these visual
+              masterpieces. Over time, I delved deeper into understanding the
+              steps involved in VFX production, from concept development and
+              storyboarding to 3D modeling, texturing, and compositing. I was
+              particularly intrigued by the combination of technical skills and
+              artistic creativity required to make seamless and believable
+              effects. During my studies and internships, I actively sought
+              opportunities to learn and apply various VFX techniques, using
+              industry-standard software like After Effects and Cinema 4D. My
+              passion for VFX grew as I practiced and experimented, constantly
+              inspired by the incredible work of VFX artists in the
+              entertainment industry. The ability to transform imagination into
+              reality through VFX continues to drive me, and I am eager to
+              contribute to creating captivating visual experiences that leave
+              audiences in awe.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="education-container" style={{ backgroundColor: theme }}>
+        <Slider
+          isVertical={true}
+          offsetLeft={"50%"}
+          isRelative={true}
+          sliderHeight={"100px"}
+          BallPos={100}
+        />
+        <h1 className="education-title" style={{ color: readableColor }}>
+          my education
+        </h1>
+        <div className="education-line" />
+        <div className="education-content-container">
+          <div className="education-content">
+            <h2 className="education-school">erasmus hogeschool brussels</h2>
+            <h3 className="education-direction">
+              <span className="bold">multimedia</span> &{" "}
+              <span className="bold"> creative technologies</span>
+            </h3>
+            <p className="education-description">
+              I pursued my studies in Multimedia and Communication Technologies
+              at Erasmus Hogeschool Brussel, where I gained a comprehensive
+              education that encompassed various facets of digital media. The
+              curriculum included web design and programming, which equipped me
+              with the skills to create responsive and visually appealing
+              websites using HTML, CSS, and JavaScript. Working in big groups
+              was a significant part of the program, fostering collaboration and
+              project management skills essential for real-world applications.
+              My passion for visual storytelling was nurtured through extensive
+              training in video editing and motion graphics, where I learned to
+              use industry-standard software like Adobe Premiere Pro and After
+              Effects. This diverse educational experience not only broadened my
+              technical abilities but also enhanced my creative thinking and
+              problem-solving skills, preparing me for a dynamic career in the
+              multimedia and communication technology fields.
+            </p>
+            <img className="education-img" src="/ehb.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="softwares-container"
+        style={{ backgroundColor: theme, color: readableColor }}
+      >
+        <div className="softwares-content">
+          <h1>
+            <span className="bold">software</span>
+            <span className="light"> I use</span>
+          </h1>
+          <div
+            className="pf-hr grid-2-span-col"
+            style={{ width: "40%", height: "1px" }}
+          />
+        </div>
+        <AnimatedDiv className="softwares-logo" staggerchildrens={true}>
+          {softwareData.map((software, index) => (
+            <SoftwareCard
+              key={software.id} // Add a unique key for each item in the array
+              logoSrc={software.logoSrc}
+              softwareName={software.softwareName}
+              type={software.type}
+              whileInView={{ y: [-50, 0] }}
+              transition={{ delay: 0.2 + index * 0.1 }}
+            />
+          ))}
+        </AnimatedDiv>
+      </div>
+      <Footer />
+    </div>
   );
 };
 
